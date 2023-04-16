4fe18fa700f8b29a40eb422c3d6ee6f1b920b0de
Samantha Alexandra <lexalaska31@gmail.com>
2023-04-16 12:18:58 -0300

    :construction: Iniciando a Página de Curriculo

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import Footer from './assets/components/static/footer/Footer'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SobreMim from './assets/pages/SobreMim/SobreMim';
 import Portfolio from './assets/pages/Portfolio/Portfolio';
+import Curriculo from './assets/pages/Curriculo/Curriculo';
 
 function App() {
   return (
     <>
        <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <div style={{ minHeight: '100vh' }}>
           <Routes> // Antigo Switch
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/sobre-mim" element={< SobreMim/>} />
             <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/curriculo" element={<Curriculo />} />
           </Routes>
         </div>
         <Footer/>
